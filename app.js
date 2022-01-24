0fdb5d7ff5caaaa32483cf6b908961cb484694da
Kusuma Ningrat <kusumanetcom@gmail.com>
2022-01-24 20:21:55 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ app.get('/', (req,res) => {
     res.send('Hello World');
 });
 
+app.get('/ready', (req,res) => {
+    res.send('The App is Ready');
+});
+
 app.listen(3000, () => {
     console.log(`Server is running on port ${3000}`)
-})
+});
 
 
